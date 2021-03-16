fc7d7d53d9ec9e239324360a1ebfa748adece085
Drq13112 <Davidredquin@gmail.com>
2021-03-16 08:41:26 +0100

    16/3/2021

---
+++ PT4.5.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 17:45:12 2021
+
+@author: david
+"""
+
+import cv2
+
+#umbral1=umbral2
+img=cv2.imread("../imagenes/llave.jpg",0)
+edgesSobel=cv2.Canny(img,100,100)
+
+import keras
